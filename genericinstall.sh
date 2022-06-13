43e8c8186caeaf1a79d6352572cbc9e6b78dd4ae
SMANTOOR <SMANTOOR@SMANTOOR-7320.idc.oracle.com>
2022-06-13 14:14:58 +0530

    Upgrade versions of 3rd party database drivers

--- genericinstall.sh
+++ genericinstall.sh
@@ -104,15 +104,15 @@ function testJDBCDrivers()
 {
 
 # Temporarily added for test
-ls /u01/app/wls/install/oracle/middleware/oracle_home/wlserver/server/lib/postgresql-42.2.8.jar
+ls /u01/app/wls/install/oracle/middleware/oracle_home/wlserver/server/lib/postgresql-42.3.6.jar
 if [[ $? != 0 ]]; then
-   echo Downloading postgresql-42.2.8.jar failed
+   echo Downloading postgresql-42.3.6.jar failed
    exit 1
 fi
 
-ls /u01/app/wls/install/oracle/middleware/oracle_home/wlserver/server/lib/mssql-jdbc-7.4.1.jre8.jar
+ls /u01/app/wls/install/oracle/middleware/oracle_home/wlserver/server/lib/mssql-jdbc-10.2.1.jre8.jar
 if [[ $? != 0 ]]; then
-   echo Downloading mssql-jdbc-7.4.1.jre8.jar failed
+   echo Downloading mssql-jdbc-10.2.1.jre8.jarr failed
    exit 1
 fi
 
@@ -121,7 +121,7 @@ fi
 function modifyWLSClasspath()
 {
   echo "Modify WLS CLASSPATH ...."
-  sed -i 's;^WEBLOGIC_CLASSPATH=\"${JAVA_HOME}.*;&\nWEBLOGIC_CLASSPATH="${WL_HOME}/server/lib/postgresql-42.2.8.jar:${WL_HOME}/server/lib/mssql-jdbc-7.4.1.jre8.jar:${WEBLOGIC_CLASSPATH}";' ${WL_HOME}/../oracle_common/common/bin/commExtEnv.sh
+  sed -i 's;^WEBLOGIC_CLASSPATH=\"${JAVA_HOME}.*;&\nWEBLOGIC_CLASSPATH="${WL_HOME}/server/lib/postgresql-42.3.6.jar:${WL_HOME}/server/lib/mssql-jdbc-10.2.1.jre8.jar:${WEBLOGIC_CLASSPATH}";' ${WL_HOME}/../oracle_common/common/bin/commExtEnv.sh
   sed -i 's;^WEBLOGIC_CLASSPATH=\"${JAVA_HOME}.*;&\n\n#**WLSAZURECUSTOMSCRIPTEXTENSION** Including Postgresql and MSSSQL JDBC Drivers in Weblogic Classpath;' ${WL_HOME}/../oracle_common/common/bin/commExtEnv.sh
   echo "Modified WLS CLASSPATH."
 }
@@ -329,10 +329,10 @@ fi
 
 export WLS_VER=$wlsversion
 export WEBLOGIC_DEPLOY_TOOL=https://github.com/oracle/weblogic-deploy-tooling/releases/download/weblogic-deploy-tooling-1.8.1/weblogic-deploy.zip
-export POSTGRESQL_JDBC_DRIVER_URL=https://jdbc.postgresql.org/download/postgresql-42.2.8.jar 
+export POSTGRESQL_JDBC_DRIVER_URL=https://jdbc.postgresql.org/download/postgresql-42.3.6.jar
 export POSTGRESQL_JDBC_DRIVER=${POSTGRESQL_JDBC_DRIVER_URL##*/}
 
-export MSSQL_JDBC_DRIVER_URL=https://repo.maven.apache.org/maven2/com/microsoft/sqlserver/mssql-jdbc/7.4.1.jre8/mssql-jdbc-7.4.1.jre8.jar
+export MSSQL_JDBC_DRIVER_URL=https://repo.maven.apache.org/maven2/com/microsoft/sqlserver/mssql-jdbc/10.2.1.jre8/mssql-jdbc-10.2.1.jre8.jar
 export MSSQL_JDBC_DRIVER=${MSSQL_JDBC_DRIVER_URL##*/}
 
 #add oracle group and user
